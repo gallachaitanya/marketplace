c9ea1b1beda0ecb497e3a171c136d3c6db7562da
Chaitanya Galla <gallachaitanya311@gmail.com>
2022-11-14 13:12:57 -0600

    Review-Product

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -13,7 +13,7 @@ import MessageBox from "../components/BoxMessage";
 import { Store } from "../Store";
 import { getError } from "../utils";
 import { toast } from "react-toastify";
-import  Button  from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -31,18 +31,18 @@ function reducer(state, action) {
       return { ...state, loadingPay: false };
     case "PAY_RESET":
       return { ...state, loadingPay: false, successPay: false };
-      case 'DELIVER_REQUEST':
-        return { ...state, loadingDeliver: true };
-      case 'DELIVER_SUCCESS':
-        return { ...state, loadingDeliver: false, successDeliver: true };
-      case 'DELIVER_FAIL':
-        return { ...state, loadingDeliver: false };
-      case 'DELIVER_RESET':
-        return {
-          ...state,
-          loadingDeliver: false,
-          successDeliver: false,
-        };
+    case "DELIVER_REQUEST":
+      return { ...state, loadingDeliver: true };
+    case "DELIVER_SUCCESS":
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case "DELIVER_FAIL":
+      return { ...state, loadingDeliver: false };
+    case "DELIVER_RESET":
+      return {
+        ...state,
+        loadingDeliver: false,
+        successDeliver: false,
+      };
     default:
       return state;
   }
@@ -55,14 +55,24 @@ export default function OrderScreen() {
   const { id: orderId } = params;
   const navigate = useNavigate();
 
-  const [{ loading, error, order, successPay, loadingPay, loadingDeliver, successDeliver }, dispatch] =
-    useReducer(reducer, {
-      loading: true,
-      order: {},
-      error: "",
-      successPay: false,
-      loadingPay: false,
-    });
+  const [
+    {
+      loading,
+      error,
+      order,
+      successPay,
+      loadingPay,
+      loadingDeliver,
+      successDeliver,
+    },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: "",
+    successPay: false,
+    loadingPay: false,
+  });
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
@@ -119,13 +129,18 @@ export default function OrderScreen() {
     if (!userInfo) {
       return navigate("/signin");
     }
-    if (!order._id || successPay || successDeliver || (order._id && order._id !== orderId)) {
+    if (
+      !order._id ||
+      successPay ||
+      successDeliver ||
+      (order._id && order._id !== orderId)
+    ) {
       fetchOrder();
       if (successPay) {
         dispatch({ type: "PAY_RESET" });
       }
       if (successDeliver) {
-        dispatch({ type: 'DELIVER_RESET' });
+        dispatch({ type: "DELIVER_RESET" });
       }
     } else {
       const loadPaypalScript = async () => {
@@ -143,11 +158,19 @@ export default function OrderScreen() {
       };
       loadPaypalScript();
     }
-  }, [order, userInfo, orderId, navigate, paypalDispatch, successPay, successDeliver]);
+  }, [
+    order,
+    userInfo,
+    orderId,
+    navigate,
+    paypalDispatch,
+    successPay,
+    successDeliver,
+  ]);
 
   async function deliverOrderHandler() {
     try {
-      dispatch({ type: 'DELIVER_REQUEST' });
+      dispatch({ type: "DELIVER_REQUEST" });
       const { data } = await axios.put(
         `/api/orders/${order._id}/deliver`,
         {},
@@ -155,11 +178,11 @@ export default function OrderScreen() {
           headers: { authorization: `Bearer ${userInfo.token}` },
         }
       );
-      dispatch({ type: 'DELIVER_SUCCESS', payload: data });
-      toast.success('Order is delivered');
+      dispatch({ type: "DELIVER_SUCCESS", payload: data });
+      toast.success("Order is delivered");
     } catch (err) {
       toast.error(getError(err));
-      dispatch({ type: 'DELIVER_FAIL' });
+      dispatch({ type: "DELIVER_FAIL" });
     }
   }
 
@@ -287,8 +310,8 @@ export default function OrderScreen() {
                 {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
                   <ListGroup.Item>
                     {loadingDeliver && <LoadingBox></LoadingBox>}
-                    <div className="d-grid">
-                      <Button type="button" onClick={deliverOrderHandler}>
+                    <div className='d-grid'>
+                      <Button type='button' onClick={deliverOrderHandler}>
                         Deliver Order
                       </Button>
                     </div>
